dd5f99773e7dc94870ad741cf4fbf1026fc21f1b
sunnyeureka <sunny.dey420@gmail.com>
2021-05-27 18:58:56 +0530

    Updates

---
+++ precache-manifest.464087e96e814e6e42e8998d9de023bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c59365b91f6041dd24dd",
+    "url": "/portfolio/static/css/main.fa5b0228.chunk.css"
+  },
+  {
+    "revision": "c59365b91f6041dd24dd",
+    "url": "/portfolio/static/js/main.c59365b9.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "912a407e5c265d4bc7688b3b5848bb7e",
+    "url": "/portfolio/index.html"
+  }
+];
